393aca1c1c9674a9c2390eba7037e2acd7eb1b94
JGABREU2004 <203727025+JGABREU2004@users.noreply.github.com>
2025-03-25 00:09:52 +0000

    Testando

---
+++ N3_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    unsigned long int NumeroGrandePositico = 4000000000;
+    unsigned int NumeroPositivo = 4000000000;
+    long int NumeroGrande = 4000000000;
+    int Numero = 4000000000;
+
+    printf("Número Grande Positivo: %lu\n", NumeroGrandePositico);
+    printf("Número Positivo: %u\n", NumeroPositivo);
+    printf("Número Grande: %ld\n", NumeroGrande);
+    printf("Número: %d\n", Numero);
+
+    return 0;
+    
+
+}
